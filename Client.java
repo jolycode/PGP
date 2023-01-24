0480957c5c0cdee3fad8355e55cdf977e7edbe3a
just Daniel <danielfedorov80@gmail.com>
2023-01-24 14:04:46 +0300

    PGP
    PGP

---
+++ Client.java
@@ -0,0 +1,121 @@
+package security_project;
+import java.io.*;
+import java.util.*;
+import java.math.*;
+import java.security.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.crypto.*;
+
+import java.util.zip.GZIPOutputStream;
+public class Client {
+    private static DataOutputStream dataOutputStream = null;
+    private static DataInputStream dataInputStream = null;
+    static Cipher encryptCipher, decryptCipher;
+    int keySize = 2048;
+    public static void main(String[] args) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	String ip="127.0.0.1";int port=990;
+    	System.out.println("Enter the name of the file to be sent");
+    	String fileName = sc.nextLine(); 
+    	String path="C:/Users/danil/CSE_ALL/439sec/"+fileName;
+        try(Socket socket = new Socket(ip,port)) {
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());   
+            PGP(path); //path
+            dataInputStream.close();
+            System.out.println("File is sent");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void PGP(String path) throws Exception {
+        String input = new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+        //Generating server keys
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);  
+        KeyPairGenerator keyPairGenerator2 = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator2.initialize(2048);//Generating client keys
+        KeyPair keyPair1=keyPairGenerator.genKeyPair();
+        KeyPair keyPair2=keyPairGenerator.genKeyPair();
+        PrivateKey senderPrivateKey =  keyPair1.getPrivate();//
+        PublicKey receiverPubKey =  keyPair2.getPublic();// get server public key
+        try (FileOutputStream out = new FileOutputStream("private.key")) {
+            out.write(keyPair2.getPrivate().getEncoded());
+        }
+        try (FileOutputStream out = new FileOutputStream("public.pub")) {
+            out.write(keyPair1.getPublic().getEncoded() );
+        }
+        String checksum;
+    	//Generating SHA-512 hash of original message
+        MessageDigest digest=MessageDigest.getInstance("SHA-512");
+        digest.reset();
+        digest.update(input.getBytes("utf8"));
+        checksum = String.format("%040x", new BigInteger(1, digest.digest()));
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE,senderPrivateKey);
+        byte[] utf8 =cipher.doFinal(checksum.getBytes("UTF-8"));
+        //Encrypt the message hash with sender private keys -> Digital Signature
+        String encryptedPrivateHash= Base64.getEncoder().encodeToString(utf8);
+      //Append original message and encrypted hash
+        String beforeZip[]={input,encryptedPrivateHash};
+        String afterZip[]=new String[beforeZip.length];
+        for(int i=0;i<beforeZip.length;i++){
+            ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream(beforeZip[i].length());
+            GZIPOutputStream gZip=new GZIPOutputStream(byteArrayOutputStream);
+            gZip.write(beforeZip[i].getBytes());
+            gZip.close();
+            byte[] compressed=byteArrayOutputStream.toByteArray();
+            byteArrayOutputStream.close();
+            afterZip[i]=Base64.getEncoder().encodeToString(compressed);
+        }
+        //Encrypt zipstring with DES
+        SecretKey key=KeyGenerator.getInstance("DES").generateKey();
+        String afterZipDES[]=new String[afterZip.length+1];
+        for(int i=0;i<afterZip.length;i++){
+            encryptCipher = Cipher.getInstance("DES");
+            encryptCipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] utf8str =afterZip[i].getBytes("UTF8");
+            byte[] encrypted = encryptCipher.doFinal(utf8str);
+            afterZipDES[i]=Base64.getEncoder().encodeToString(encrypted);
+        }
+      //Encrypt DES key with Receiver Public Key using RSA
+            String encodedKey=Base64.getEncoder().encodeToString(key.getEncoded());
+            Cipher cipher2 = Cipher.getInstance("RSA");
+            cipher2.init(Cipher.ENCRYPT_MODE, receiverPubKey);  
+            byte[] utf8new2 = cipher2.doFinal(encodedKey.getBytes("UTF-8"));
+          //SecretKey is base64 encoded 
+            String encryptedKey=Base64.getEncoder().encodeToString(utf8new2);
+          //Decrypting DES key with Receiver Private Key using RSA
+            afterZipDES[2]=encryptedKey;
+            String messageToServer[]=afterZipDES;
+            sendFile(messageToServer);
+    }
+    private static void sendFile(String array[]) throws Exception{
+        for(int i = 0 ; i < 3 ; i++){
+            System.out.println(array[i]);//messageToServer --> afterZipDES
+        }
+        byte[] data=array[0].getBytes("UTF-8");
+        dataOutputStream.writeInt(data.length);
+        dataOutputStream.write(data);
+        byte[] hash=array[1].getBytes("UTF-8"); 
+        dataOutputStream.writeInt(hash.length);
+        dataOutputStream.write(hash);// send hash
+        byte[] key=array[2].getBytes("UTF-8"); 
+        dataOutputStream.writeInt(key.length);
+        dataOutputStream.write(key); 
+    } 
+}
+/*
+client writes message to server.
+
+message provided as SHA-512 hash 
+hash signed by RSA,hash is encrypted by private keys of client. The inputs to RSA are Private Keys of client and hash.output from RSA is digitally signed hash or encrypted hash .
+
+output encrypted by DES
+
+message encrypted using SecretKey,sent server.Encrypt SecretKey of DES with server PublicKey.use RSA for this and the inputs to it will be the Public key of server and SecretKey.
+*/
+
---
+++ Server.java
@@ -0,0 +1,130 @@
+package security_project;
+import java.io.*;
+import java.util.*;
+import java.net.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.zip.GZIPInputStream;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+public class Server {
+	 static Cipher ecipher, dcipher;
+	    private static DataOutputStream dataOutputStream = null;
+	    private static DataInputStream dataInputStream = null;
+	    public static void main(String[] args) {
+	    	int port=990;
+	        try(ServerSocket serverSocket = new ServerSocket(port)){
+	            System.out.println("Listenning on port:"+port);
+	            Socket clientSocket = serverSocket.accept();
+	            System.out.println(" Client connected: "+clientSocket);
+	            dataInputStream = new DataInputStream(clientSocket.getInputStream());
+	            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());     
+	            /*receiving file                      */
+		        int length=dataInputStream.readInt();
+		        byte[] data_=new byte[length];
+		        dataInputStream.readFully(data_);
+		        String data =new String(data_,"UTF-8");
+		        System.out.println("data:"+data+"\n\n");
+		        int length2=dataInputStream.readInt();
+		        byte[] hash_=new byte[length2];
+		        dataInputStream.readFully(hash_);
+		        String hash=new String(hash_,"UTF-8");
+		        System.out.println("Hash:"+hash+"\n\n");
+		        int length3=dataInputStream.readInt();
+		        byte[] key_=new byte[length3];
+		        dataInputStream.readFully(key_);
+		        String key=new String(key_,"UTF-8");
+		        PGP(data,hash,key);   
+	            /*received            */	          
+	            dataInputStream.close();
+	            dataOutputStream.close();
+	            clientSocket.close();
+	        } catch (Exception e){
+	            e.printStackTrace();
+	        }
+	    }
+	    private static void PGP(String data, String hash, String key) throws Exception {
+	        String[] message={data,hash,key};
+	    	//Receiver receives the message as secret key encrypted with receiver pub key
+	        byte[] bytes = Files.readAllBytes(Paths.get("public.pub"));
+	    	//Receiver decrypts the  with  privatekey
+	        X509EncodedKeySpec ks = new X509EncodedKeySpec(bytes);
+	        KeyFactory kf = KeyFactory.getInstance("RSA");
+	        PublicKey publicKey = kf.generatePublic(ks);
+	        byte[] bytes2 = Files.readAllBytes(Paths.get("private.key"));
+	        PKCS8EncodedKeySpec ks2 = new PKCS8EncodedKeySpec(bytes2);
+	        KeyFactory kf2 = KeyFactory.getInstance("RSA");
+	        PrivateKey privateKey = kf2.generatePrivate(ks2);
+	        //Key after decryption is in base64 encoded form
+	        byte[] encrypted=Base64.getDecoder().decode(key);
+	        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+	        cipher.init(Cipher.DECRYPT_MODE,privateKey);
+			byte[] utf8 = cipher.doFinal(encrypted);
+	        String eRecevierSecretKey=new String(utf8,"UTF8");
+	        byte[] dReceiverSecretKey = Base64.getDecoder().decode(eRecevierSecretKey);
+	        SecretKey originalKey = new SecretKeySpec(dReceiverSecretKey, 0, dReceiverSecretKey.length, "DES");
+	        //Decrypt the rest of the message in messagetoreceiver with SecretKey originalKey
+			String receiverdEcryptedMessage[] = new String[message.length-1];
+			
+	        dcipher = Cipher.getInstance("DES");
+		    dcipher.init(Cipher.DECRYPT_MODE, originalKey);
+	        for(int i=0;i<message.length-1;i++){
+	        // Decode base64 to get bytes
+	            byte[] dec=Base64.getDecoder().decode(message[i]);
+	            byte[] utf8_ = dcipher.doFinal(dec);  
+	         // Decode using utf-8
+	            message[i]= new String(utf8_,"UTF8");
+	        } 
+	        String unzippedString[]=new String[receiverdEcryptedMessage.length]; //Unziping
+	        File fout = new File("C:/Users/danil/CSE_ALL/439sec/server_output/new.txt"); //new file path
+	        FileOutputStream fos = new FileOutputStream(fout);
+	        for(int i=0;i<unzippedString.length;i++){//compress
+	            byte[] compressed=Base64.getDecoder().decode(message[i]);
+	        	ByteArrayInputStream bis = new ByteArrayInputStream(compressed);
+	            GZIPInputStream gis = new GZIPInputStream(bis);
+	            BufferedReader br = new BufferedReader(new InputStreamReader(gis, "UTF-8"));
+	            StringBuilder sb = new StringBuilder();
+	            String line;
+	            while((line = br.readLine()) != null) {
+	                if(i==0){
+	                    fos.write(line.getBytes("utf8"));
+	                    fos.write(10);
+	                }
+	                sb.append(line);
+	            }
+	            fos.close();
+	            br.close();
+	            gis.close();
+	            bis.close();
+	            unzippedString[i]=sb.toString();  
+	         }
+	         byte[] encrypted2=Base64.getDecoder().decode(unzippedString[1]);
+	         cipher.init(Cipher.DECRYPT_MODE,publicKey);
+	         byte[] utf8_2 = cipher.doFinal(encrypted2);
+	       //Message has been received and is in unzipstring but check the digital signature of the sender i.e. verify the hash with senderpubkey
+	      //decrypting the encrypted hash in unzipstring with sender pub key
+	         String receivedHash=new String(utf8_2,"UTF8");
+	     	//Calculating SHA512 at receiver side of message
+	         MessageDigest digest = MessageDigest.getInstance("SHA-512");
+			 digest.reset();
+			 digest.update(unzippedString[0].getBytes("utf8"));
+			 String calculatedHash = String.format("%040x", new BigInteger(1, digest.digest()));
+	         if(receivedHash.equalsIgnoreCase(calculatedHash)) { //authentication
+	            System.out.println("File is Received.\n");
+	         }
+	    }
+}
+//decrypt the SecretKey of DES with his Private Keys
+//inputs to RSA algorithm  will be Private Keys. The output from RSA will give  the SecretKey.
+//zip
+/*From  above step,will get signed hash and the original message.verify signed hash.For this,calculate the hash of the original message by SHA-512.
+also decrypt signed hash with the public keys of client by using RSA. */
+//Compare the decrypted_hash and calculated_hash. If they turn out to be the same, then authentication is achieved which means that the message was indeed sent by client.
